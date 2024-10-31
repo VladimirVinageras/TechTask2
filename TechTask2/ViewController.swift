cd89dedaa63f66e2e4fc99b360c34d8c15f0e9ad
Vladimir Vinageras <vvinageras@gmail.com>
2024-10-31 11:00:45 +0200

    Initial Commit

---
+++ TechTask2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechTask2
+//
+//  Created by Vladimir Vinakheras on 31.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
